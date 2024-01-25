4832f41ec35da05c40945b1465fb20ff54214f13
Athauan <athauan.marques@gmail.com>
2024-01-25 15:30:58 -0300

    fix: contatos, alinhamento desing mobile

--- js/mail.js
+++ js/mail.js
@@ -1,10 +1,22 @@
-function submit() {
-  $("#target").unbind('submit').submit()
-}
-
 $(document).ready(function () {
-  $('#target').on('submit', function (e) {
-      e.preventDefault();
-      $('#captcha').show();
-  });
+    // Intercepta o evento de envio do formulário
+    $('#target').submit(function (event) {
+        // Evita o envio padrão do formulário
+        event.preventDefault();
+
+        // Realize a lógica de verificação do envio aqui (pode ser uma chamada AJAX, etc.)
+
+        // Simulando um envio bem-sucedido para teste
+        var envioBemSucedido = true;
+
+        if (envioBemSucedido) {
+            // Exibe o modal de sucesso
+            $('#SucessoEnvio').modal('show');
+            // Limpa os campos do formulário
+            $('#target')[0].reset();
+        } else {
+            // Exibe o modal de erro
+            $('#ErroEnvio').modal('show');
+        }
+    });
 });
